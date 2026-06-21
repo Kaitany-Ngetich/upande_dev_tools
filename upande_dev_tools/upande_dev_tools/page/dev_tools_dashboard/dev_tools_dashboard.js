6c870da01fa6b0a3df3f9ed2439e0f02e0e8d8da
Shadrack Kaitany <kaitanyshadrack@gmail.com>
2026-06-21 18:50:46 +0300

    Tools Dashboard deleted

--- upande_dev_tools/upande_dev_tools/page/dev_tools_dashboard/dev_tools_dashboard.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['dev_tools_dashboard'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Tools Dashboard',
-		single_column: true
-	});
-}
